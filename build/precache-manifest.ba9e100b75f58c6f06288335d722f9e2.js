3dae995b00db728b566fcd256cc632d5e41baa1a
Tambu Ebuta <tambu.ebuta@gmail.com>
2019-09-23 16:24:59 +0100

    initial commit

---
+++ build/precache-manifest.ba9e100b75f58c6f06288335d722f9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d29d4377d7e0ddb151b93ed15f18f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f2b867914e6d54b37a9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "20826a6408ce95dc8ca8",
+    "url": "/static/js/2.251f9854.chunk.js"
+  },
+  {
+    "revision": "7f2b867914e6d54b37a9",
+    "url": "/static/js/main.46dca153.chunk.js"
+  },
+  {
+    "revision": "65a81b1ca9a7620b8ceb",
+    "url": "/static/js/runtime-main.1dc4db23.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
